441121c95c1baaf4f10aff2a6d6d4bfc5f6fa642
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-04-14 10:51:03 +0300

    Whatsapp

--- resources/views/user/user.blade.php
+++ resources/views/user/user.blade.php
@@ -52,5 +52,19 @@
 			<script src="/frontend/js/owl.carousel.min.js"></script>									
 			<script src="/frontend/js/mail-script.js"></script>	
 			<script src="/frontend/js/main.js"></script>
+			<script type="text/javascript">
+				(function () {
+					var options = {
+						whatsapp: "+254(721) 890-969", // WhatsApp number
+						call_to_action: "Message us", // Call to action
+						button_color: "#FF6550", // Color of button
+						position: "right", // Position may be 'right' or 'left'
+					};
+					var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+					var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+					s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+					var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+				})();
+			</script>
 		</body>
 	</html>
