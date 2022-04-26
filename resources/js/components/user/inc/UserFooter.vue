5fe807ba55b91735ec591900da623ce9e123cc21
Joel Rupiah <rupiahjeremiah@gmail.com>
2022-04-26 23:54:27 +0300

    footer

--- resources/js/components/user/inc/UserFooter.vue
+++ resources/js/components/user/inc/UserFooter.vue
@@ -49,7 +49,7 @@
 					<div class="footer-bottom row align-items-center justify-content-between">
 						<p class="footer-text m-0 col-lg-6 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 <!-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> &amp; distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a> -->
-							Copyright Pinecrest Academy. All rights reserved
+							Copyright Pinecrest Academy 2022. All rights reserved
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
 						<div class="col-lg-6 col-sm-12 footer-social">
 							<a href="#"><i class="fa fa-facebook"></i></a>
